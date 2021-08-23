55f02fc1d287fc1826da5c5b11cdca2b74b734a5
ccizer <can.cizer@gmail.com>
2021-08-23 18:46:00 +0200

    CorrelationSimulation

---
+++ src/test/scala/simulations/CorrelationSimulation.scala
@@ -0,0 +1,39 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration.DurationInt
+
+class CorrelationSimulation extends Simulation {
+
+  //1. HTTP Configuration
+  val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl("https://jsonplaceholder.typicode.com")
+    .header("Accept", "application/json")
+
+  //2. Scenario Definition
+  val scn: ScenarioBuilder = scenario("Second Scenario")
+
+    // First, Check the title of the first post
+    .exec(http("Get Specific Post")
+      .get("/posts/1")
+      .check(jsonPath("$.title").is("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")))
+
+    // Second, Get the id of the second element in the posts list and save it
+    .exec(http("Get All Post")
+      .get("/posts")
+      .check(jsonPath("$[1].id").saveAs("postId")))
+
+    // Third, Check the title of the saved post id
+    .exec(http("Get Specific Post")
+      .get("/posts/${postId}")
+      .check(jsonPath("$.title").is("qui est esse")))
+
+  //3. Load Scenario
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+}
